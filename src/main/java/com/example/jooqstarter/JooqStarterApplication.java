ce058ebcc2019c018371e7406aed983879e6a22f
keede <wesaq11@gmail.com>
2023-07-22 14:47:29 +0900

    Initial commit

---
+++ src/main/java/com/example/jooqstarter/JooqStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.jooqstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JooqStarterApplication.class, args);
+    }
+
+}
