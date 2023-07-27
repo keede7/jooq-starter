5b8703df2e255629a6fe393a6eb1d44d44d1d4f6
keede <wesaq11@gmail.com>
2023-07-28 00:45:34 +0900

    feat: 이벤트리스너 사용해보기

--- src/main/java/com/example/jooqstarter/entity/MemberQueryRepository.java
+++ src/main/java/com/example/jooqstarter/entity/MemberQueryRepository.java
@@ -3,6 +3,9 @@ package com.example.jooqstarter.entity;
 import nu.studer.sample.jooqdb.tables.MemberT;
 import org.jooq.DSLContext;
 import org.jooq.impl.DSL;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,9 +14,11 @@ import java.util.List;
 public class MemberQueryRepository {
 
     private final DSLContext dsl;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
-    public MemberQueryRepository(final DSLContext dsl) {
+    public MemberQueryRepository(DSLContext dsl, ApplicationEventPublisher applicationEventPublisher) {
         this.dsl = dsl;
+        this.applicationEventPublisher = applicationEventPublisher;
     }
 
     public List<Member> getList1() {
@@ -22,9 +27,16 @@ public class MemberQueryRepository {
                 .fetchInto(Member.class);
     }
 
-    public List<Member> getList2() {
+    public List<Member> getList2(String ss) {
+        applicationEventPublisher.publishEvent(ss);
         return dsl.selectFrom(MemberT.MEMBER_T)
                 .fetchInto(Member.class);
+    }
+
+    @EventListener
+    public void eventTest(String test) {
+        System.out.println("test = " + test);
+
     }
 
 }
